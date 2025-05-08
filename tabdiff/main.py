e01b6c98be3b3164b571918eea17d0107b19ae3a
ajozefiak <adam.jozefiak@alumni.ubc.ca>
2025-05-08 14:41:04 -0400

    add a temporary comment

--- tabdiff/main.py
+++ tabdiff/main.py
@@ -222,8 +222,8 @@ def main(args):
         raw_config['diffusion_params']['scheduler'] = 'power_mean_per_column'
         raw_config['diffusion_params']['cat_scheduler'] = 'log_linear_per_column'
         # TODO: Fix the names below here
-        raw_config['diffusion_params']['scheduler'] = 'tree_num'
-        raw_config['diffusion_params']['cat_scheduler'] = 'tree_cat'
+        # raw_config['diffusion_params']['scheduler'] = 'tree_num'
+        # raw_config['diffusion_params']['cat_scheduler'] = 'tree_cat'
 
     # TODO check that this is working:
     if num_depths is not None:

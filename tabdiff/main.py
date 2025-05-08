7f8565584976485f474fd9f951095f4f9cb855a2
ajozefiak <adam.jozefiak@alumni.ubc.ca>
2025-05-08 14:48:07 -0400

    remove tensor from json

--- tabdiff/main.py
+++ tabdiff/main.py
@@ -228,10 +228,10 @@ def main(args):
     # TODO check that this is working:
     if num_depths is not None:
         raw_config["diffusion_params"]["num_depths"] = num_depths
-        raw_config['diffusion_params']['noise_schedule_params']['num_depths'] = torch.tensor(num_depths)
+        raw_config['diffusion_params']['noise_schedule_params']['num_depths'] = num_depths
     if cat_depths is not None:
         raw_config["diffusion_params"]["cat_depths"] = cat_depths
-        raw_config['diffusion_params']['noise_schedule_params']['cat_depths'] = torch.tensor(cat_depths)
+        raw_config['diffusion_params']['noise_schedule_params']['cat_depths'] = cat_depths
     if num_tree_layers is not None:
         raw_config["diffusion_params"]["num_tree_layers"] = num_tree_layers
         raw_config['diffusion_params']['noise_schedule_params']['num_tree_layers'] = num_tree_layers

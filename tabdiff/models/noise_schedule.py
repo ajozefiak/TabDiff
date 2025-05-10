170f8b90c26a95e9b306ec987f902a34d7fc584e
ajozefiak <adam.jozefiak@alumni.ubc.ca>
2025-05-10 19:46:47 -0400

    fix

--- tabdiff/models/noise_schedule.py
+++ tabdiff/models/noise_schedule.py
@@ -194,12 +194,14 @@ class TreeLayeredNoiseNum(nn.Module):
     # NEW CODE:
     # d_t: the layer that we diffusing
     # d_t = ((1-t) * self.num_tree_layers).int()
-    d_t = torch.floor(t * self.num_tree_layers).long()
-    # d_t = torch.floor((1 - t) * self.num_tree_layers).long()
-    # d_t = d_t.clamp(min=0, max=self.num_tree_layers - 1)
+    
+    d_t = torch.floor((1 - t) * self.num_tree_layers).long()
+    d_t = d_t.clamp(min=0, max=self.num_tree_layers - 1)
 
     # t_ is the shifted and normalized noise t for the layer d_t
-    t_ = t * self.num_tree_layers - d_t
+    # t_ = t * self.num_tree_layers - d_t
+
+    t_ = self.num_tree_layers * t - self.num_tree_layers + 1 + d_t
 
     # NOTE: I think that this should be correct
     sigma_max_pow_ = sigma_max_pow * (self.num_depths > d_t) + sigma_min_pow * (self.num_depths < d_t) + sigma_max_pow * (self.num_depths == d_t)

6a540037e682b38bee9d81d4f4eeb5972edc031b
ajozefiak <adam.jozefiak@alumni.ubc.ca>
2025-05-10 19:31:13 -0400

    add a comment to avoiding clamping

--- tabdiff/models/noise_schedule.py
+++ tabdiff/models/noise_schedule.py
@@ -195,7 +195,7 @@ class TreeLayeredNoiseNum(nn.Module):
     # d_t: the layer that we diffusing
     # d_t = ((1-t) * self.num_tree_layers).int()
     d_t = torch.floor((1 - t) * self.num_tree_layers).long()
-    d_t = d_t.clamp(min=0, max=self.num_tree_layers - 1)
+    # d_t = d_t.clamp(min=0, max=self.num_tree_layers - 1)
 
     # t_ is the shifted and normalized noise t for the layer d_t
     t_ = t * self.num_tree_layers - d_t

53aa8d370203c5e56a2e3c43821b15266212f30e
ajozefiak <adam.jozefiak@alumni.ubc.ca>
2025-05-13 18:18:08 -0400

    update

--- tabdiff/main.py
+++ tabdiff/main.py
@@ -69,6 +69,7 @@ def main(args):
     
     print(f"{args.mode.capitalize()} Mode is Enabled")
     num_samples_to_generate = None
+    num_samples_to_generate = args.num_samples_to_generate
     ckpt_path = None
     if args.mode == 'train':
         print("NEW training is started")
